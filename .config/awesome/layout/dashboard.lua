ad729537c3c52503a8ae384c4acf0f0ad88437d1
AllJavi <49349604+AllJavi@users.noreply.github.com>
2022-12-28 18:47:13 +0100

    dashboard style changes

--- .config/awesome/layout/dashboard.lua
+++ .config/awesome/layout/dashboard.lua
@@ -29,10 +29,9 @@ local dashboardPanel = function(s)
 
     awful.placement.maximize(dashboard)
 
-    local quotes = {
-        "<span>Remember! Reality is an illusion, the universe is a hologram, buy gold, bye!</span><span font_desc='italic'>\n\n- Bill, Gravity Falls</span>",
-        "<span>When there's no cops around, anything's legal!</span><span font_desc='italic'>\n\n- Grunkle Stan, Gravity Falls</span>"
-    }
+    local quotes =
+        {"<span>Remember! Reality is an illusion, the universe is a hologram, buy gold, bye!</span><span font_desc='italic'>\n\n- Bill, Gravity Falls</span>",
+         "<span>When there's no cops around, anything's legal!</span><span font_desc='italic'>\n\n- Grunkle Stan, Gravity Falls</span>"}
 
     local quote_textbox = wibox.widget {
         markup = '<span color="' .. beautiful.system_black_dark .. '">' .. quotes[math.random(#quotes)] .. '</span>',
@@ -44,7 +43,8 @@ local dashboardPanel = function(s)
     }
 
     local function dashboard_visibility()
-        quote_textbox:set_markup('<span color="' .. beautiful.system_black_dark .. '">' .. quotes[math.random(#quotes)] .. '</span>')
+        quote_textbox:set_markup(
+            '<span color="' .. beautiful.system_black_dark .. '">' .. quotes[math.random(#quotes)] .. '</span>')
 
         if dashboard.visible == true then
             dashboard.visible = false
@@ -66,10 +66,10 @@ local dashboardPanel = function(s)
                     {
                         nil,
                         {
-                            image         = image,
-                            resize        = true,
+                            image = image,
+                            resize = true,
                             forced_height = dpi(90),
-                            widget        = wibox.widget.imagebox
+                            widget = wibox.widget.imagebox
                         },
                         nil,
                         expand = "none",
@@ -84,14 +84,10 @@ local dashboardPanel = function(s)
             widget = clickable_container
         }
 
-        clickable_folder:buttons(
-        gears.table.join(
-        awful.button({}, 1, nil, function()
+        clickable_folder:buttons(gears.table.join(awful.button({}, 1, nil, function()
             awful.spawn(apps.default.file_manager .. ' ' .. dir)
             dashboard_visibility()
-        end)
-        )
-        )
+        end)))
 
         return clickable_folder
     end
@@ -103,10 +99,10 @@ local dashboardPanel = function(s)
                     {
                         nil,
                         {
-                            image         = icon,
-                            resize        = true,
+                            image = icon,
+                            resize = true,
                             forced_height = dpi(50),
-                            widget        = wibox.widget.imagebox
+                            widget = wibox.widget.imagebox
                         },
                         nil,
                         expand = "none",
@@ -120,71 +116,65 @@ local dashboardPanel = function(s)
             },
             widget = clickable_container
         }
-        clickable_application:buttons(
-        gears.table.join(
-        awful.button({}, 1, nil, function()
+        clickable_application:buttons(gears.table.join(awful.button({}, 1, nil, function()
             action()
             dashboard_visibility()
-        end)
-        )
-        )
+        end)))
         return clickable_application
     end
 
     -- right click to hide dashboard
-    dashboard:buttons(gears.table.join(
-    awful.button({}, 3, function()
+    dashboard:buttons(gears.table.join(awful.button({}, 3, function()
         dashboard_visibility()
-    end)
-    ))
+    end)))
 
     local distribution = wibox.widget {
-        homogeneous     = true,
+        homogeneous = true,
         forced_num_cols = 6,
         forced_num_rows = 5,
-        min_cols_size   = dpi(130),
-        min_rows_size   = dpi(130),
-        superpose       = true,
-        expand          = true,
-        spacing         = dpi(25),
-        forced_width    = dpi(910),
-        forced_height   = dpi(650),
-        layout          = wibox.layout.grid,
+        min_cols_size = dpi(130),
+        min_rows_size = dpi(130),
+        superpose = true,
+        expand = true,
+        spacing = dpi(25),
+        forced_width = dpi(910),
+        forced_height = dpi(650),
+        layout = wibox.layout.grid
     }
 
     distribution:add_widget_at({
+        {
             {
-                {
-                    folder(beautiful.code_folder, '/home/alljavi-shared/code'),
-                    widget = wibox.container.background
-                },
-                {
-                    folder(beautiful.design_folder, '/home/alljavi-shared/design'),
-                    widget = wibox.container.background
-                },
-                {
-                    folder(beautiful.download_folder, '/home/alljavi-shared/descargas'),
-                    widget = wibox.container.background
-                },
-                {
-                    folder(beautiful.teleco_folder, '/home/alljavi-shared/teleco'),
-                    widget = wibox.container.background
-                },
-                {
-                    folder(beautiful.design3d_folder, '/home/alljavi-shared/3d design'),
-                    widget = wibox.container.background
-                },
-                {
-                    folder(beautiful.shared_folder, '/home/alljavi-shared'),
-                    widget = wibox.container.background
-                },
-                layout = wibox.layout.ratio.vertical
+                folder(beautiful.code_folder, '/home/alljavi-shared/code'),
+                widget = wibox.container.background
             },
-            bg = beautiful.system_black_dark,
-            shape_border_width = beautiful.border_width,
-            shape_border_color = beautiful.system_yellow_dark,
-            shape = rounded_shape,
-            widget = wibox.container.background
+            {
+                folder(beautiful.design_folder, '/home/alljavi-shared/design'),
+                widget = wibox.container.background
+            },
+            {
+                folder(beautiful.download_folder, '/home/alljavi-shared/descargas'),
+                widget = wibox.container.background
+            },
+            {
+                folder(beautiful.teleco_folder, '/home/alljavi-shared/teleco'),
+                widget = wibox.container.background
+            },
+            {
+                folder(beautiful.design3d_folder, '/home/alljavi-shared/3d design'),
+                widget = wibox.container.background
+            },
+            {
+                folder(beautiful.shared_folder, '/home/alljavi-shared'),
+                widget = wibox.container.background
+            },
+            layout = wibox.layout.ratio.vertical
+        },
+        bg = beautiful.system_black_dark,
+        shape_border_width = beautiful.border_width,
+        shape_border_color = beautiful.system_yellow_dark,
+        shape = rounded_shape,
+        widget = wibox.container.background
     }, 1, 1, 4, 1)
 
     local profile_widget = {
@@ -196,13 +186,15 @@ local dashboardPanel = function(s)
                     {
                         {
                             {
-                                image      = beautiful.user_picture,
-                                resize     = true,
-                                widget     = wibox.widget.imagebox
+                                image = beautiful.user_picture,
+                                resize = true,
+                                widget = wibox.widget.imagebox
                             },
                             shape = gears.shape.circle,
                             forced_width = dpi(160),
                             forced_height = dpi(160),
+                            shape_border_width = beautiful.border_width,
+                            shape_border_color = beautiful.background,
                             opacity = 1.0,
                             widget = wibox.container.background
                         },
@@ -247,12 +239,13 @@ local dashboardPanel = function(s)
                 widget.align = 'center'
                 widget.valign = 'center'
                 -- widget.forced_width = dpi()
-                widget.markup = ('<span font_desc="" color=\'' .. beautiful.system_green_dark .. '\'>' .. line .. '</span>')
+                widget.markup = ('<span font_desc="" color=\'' .. beautiful.system_green_dark .. '\'>' .. line ..
+                                    '</span>')
                 return
             end
         end),
         expand = 'outside',
-        layout = wibox.layout.align.horizontal,
+        layout = wibox.layout.align.horizontal
     }
 
     local hour_widget = {
@@ -270,12 +263,12 @@ local dashboardPanel = function(s)
     second_box:ajust_ratio(2, 0.75, 0.25, 0.0)
 
     distribution:add_widget_at({
-            second_box,
-            bg = beautiful.background,
-            -- shape_border_width = beautiful.border_width,
-            -- shape_border_color = beautiful.system_green_dark,
-            shape = rounded_shape,
-            widget = wibox.container.background
+        second_box,
+        bg = beautiful.background,
+        -- shape_border_width = beautiful.border_width,
+        -- shape_border_color = beautiful.system_green_dark,
+        shape = rounded_shape,
+        widget = wibox.container.background
     }, 1, 2, 3, 2)
 
     distribution:add_widget_at(wibox.widget {
@@ -353,48 +346,48 @@ local dashboardPanel = function(s)
     -- }, 3, 6, 1, 1)
 
     distribution:add_widget_at({
+        {
             {
-                {
-                    quote_textbox,
-                    margins = 30,
-                    widget = wibox.container.margin
-                },
-                bg = beautiful.system_green_dark .. 'EE',
-                widget = wibox.container.background
-            },
-            bg = beautiful.system_black_dark,
-            -- shape_border_width = beautiful.border_width,
-            -- shape_border_color = beautiful.system_green_dark,
-            shape = rounded_shape,
-            widget = wibox.container.background
-    }, 4, 2, 2, 3)
-
-    distribution:add_widget_at({
-            {
-                {
-                    nil,
-                    wibox.widget {
-                        date         = os.date('*t'),
-                        font         = 'Monospace 14',
-                        spacing      = 12,
-                        week_numbers = false,
-                        start_sunday = false,
-                        widget       = wibox.widget.calendar.month
-                    },
-                    nil,
-                    layout = wibox.layout.align.horizontal
-                },
+                quote_textbox,
                 margins = 30,
                 widget = wibox.container.margin
             },
-            bg = beautiful.system_black_dark,
-            shape_border_width = beautiful.border_width,
-            shape_border_color = beautiful.system_yellow_dark,
-            shape = rounded_shape,
+            bg = beautiful.system_green_dark .. 'EE',
             widget = wibox.container.background
+        },
+        bg = beautiful.system_black_dark,
+        -- shape_border_width = beautiful.border_width,
+        -- shape_border_color = beautiful.system_green_dark,
+        shape = rounded_shape,
+        widget = wibox.container.background
+    }, 4, 2, 2, 3)
+
+    distribution:add_widget_at({
+        {
+            {
+                nil,
+                wibox.widget {
+                    date = os.date('*t'),
+                    font = 'Monospace 14',
+                    spacing = 12,
+                    week_numbers = false,
+                    start_sunday = false,
+                    widget = wibox.widget.calendar.month
+                },
+                nil,
+                layout = wibox.layout.align.horizontal
+            },
+            margins = 30,
+            widget = wibox.container.margin
+        },
+        bg = beautiful.system_black_dark,
+        shape_border_width = beautiful.border_width,
+        shape_border_color = beautiful.system_yellow_dark,
+        shape = rounded_shape,
+        widget = wibox.container.background
     }, 1, 4, 2, 2)
 
-    dashboard:setup {
+    dashboard:setup{
         nil,
         {
             nil,
